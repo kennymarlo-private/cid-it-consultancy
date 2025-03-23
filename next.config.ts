44a74e1b995ab6b00a0bf0555f2de68e0efd5ccd
Kenny Cid <kcid@ciditconsultancy.com>
2025-03-23 21:16:20 +0800

    Update CSP Headers

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googletagmanager.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
+    img-src 'self' blob: data: https://*.google-analytics.com https://*.googletagmanager.com;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
+    connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://google.com https://www.google.com;
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
