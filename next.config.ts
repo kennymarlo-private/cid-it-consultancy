46867676cee03ffddca17fc58ec7dce7426a911c
Kenny Cid <kcid@ciditconsultancy.com>
2025-03-23 23:18:19 +0800

    GA4 update

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googletagmanager.com;
-    style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://*.google-analytics.com https://*.googletagmanager.com;
-    font-src 'self' 'unsafe-eval' https://fonts.gstatic.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com;
+    style-src 'self' 'unsafe-inline' 'unsafe-eval' fonts.googleapis.com;
+    img-src 'self' blob: data:;
+    font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com;
     object-src 'none';
     base-uri 'self';
-    connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://google.com https://www.google.com;
+    connect-src 'self';
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
-import { headers } from 'next/headers'
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import React from "react";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "CID IT CONSULTANCY - IT SOLUTIONS",
