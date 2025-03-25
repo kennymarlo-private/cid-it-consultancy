442067a0ea7398ce97120a3d868d690692ef7dfb
Kenny Cid <kcid@ciditconsultancy.com>
2025-03-25 18:49:11 +0800

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default  function RootLayout({children }: Readonly<{ children: React.Reac
         <Analytics />
         <Footer />
         </body>
-        <GoogleAnalytics gaId="G-KB4RRDK878" />
+        <GoogleAnalytics gaId={"G-KB4RRDK878"} />
         </html>
     );
 }
