430fab804eb1597b9c51f493501f123abb0b9671
Kenny Cid <kcid@ciditconsultancy.com>
2025-03-25 18:47:23 +0800

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from '@vercel/analytics/next';
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
@@ -35,6 +36,7 @@ export default  function RootLayout({children }: Readonly<{ children: React.Reac
         <body  className="bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white font-roboto">
         <Header />
         {children}
+        <Analytics />
         <Footer />
         </body>
         <GoogleAnalytics gaId="G-KB4RRDK878" />
