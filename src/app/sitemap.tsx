446193593f88e24489db494684ef5205369e030e
Kenny Cid <kcid@ciditconsultancy.com>
2025-08-13 12:42:18 +0800

    update sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,35 +1,60 @@
 import type { MetadataRoute } from 'next'
+
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://ciditconsultancy.com.com',
+            url: 'https://ciditconsultancy.com',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://ciditconsultancy.com.com/about',
+            url: 'https://ciditconsultancy.com/about',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://ciditconsultancy.com.com/services',
+            url: 'https://ciditconsultancy.com/services',
             lastModified: new Date(),
             changeFrequency: 'weekly',
-            priority: 0.5,
+            priority: 0.9,
         },
         {
-            url: 'https://ciditconsultancy.com.com/showcase',
+            url: 'https://ciditconsultancy.com/contact',
             lastModified: new Date(),
             changeFrequency: 'weekly',
-            priority: 0.5,
+            priority: 0.8,
         },
         {
-            url: 'https://ciditconsultancy.com.com/contact',
+            url: 'https://ciditconsultancy.com/faq',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://ciditconsultancy.com/blog',
             lastModified: new Date(),
             changeFrequency: 'weekly',
-            priority: 0.5,
+            priority: 0.6,
+        },
+        {
+            url: 'https://ciditconsultancy.com/showcase',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.6,
+        },
+        {
+            url: 'https://ciditconsultancy.com/terms-of-service',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
+        {
+            url: 'https://ciditconsultancy.com/privacy-policy',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
         },
     ]
 }
